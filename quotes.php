7909a7e74756989cf6e77ac2562b3557f8445ecc
Jamie Wilkinson <jamie@tramchase.com>
2010-07-30 00:01:51 -0700

    add Twitter quotes and links for all original blog quotes

--- quotes.php
+++ quotes.php
@@ -1,29 +1,37 @@
 <?php
                      $quotes = array(
-                                "HAS ANYONE EVEN TAKEN THE TIME TO AT LEAST DO THE MATH???",
-                                "AT THAT POINT WE'RE RACING AGAINST THE SUN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
-                                "CALL ME WHAT YOU WANT BUT NEVER SAY I DIDN'T GIVE MY ALL!!!",
-                                "ALL I CARE ABOUT ARE THE FANS.",
-                                "I'm typing so fucking hard I might break my fucking Mac book Air!!!!!!!!",
-                                "I am sick of negative people who just sit around trying 2 plot my downfall... Why????",
-                                "I'VE BEEN ATTACKED FOR BEING ME... FOR BEING BRIGHT RED IN A GREY WORLD.",
-                                "I AM NUCLEAR ENERGY",
-                                "YOUR PESSIMISM IS MY POWER... YOUR PRESUMPTIONS LEAD TO MY REDEMPTIONS...",
-                                "YOU OBVIOUSLY DON'T KNOW WHO YOU'RE UP AGAINST",
-                                // "I'm the fucking end-all, be-all of music",
-                                // "My greatest pain in life is that I will never be able to see myself perform",
-                                "I'VE BEEN LOVED AND HATED... I'VE BEEN HAILED AND RIDICULED...",
-                                "I WILL NEVER ATTACK NEGATIVE PEOPLE AND THEIR ENERGY...",
-                                "IT IS THE CRACK IN THE WALL THAT ALLOWS LIGHT TO SHINE THROUGH... SOOO THANK YOU SENSATIONAL NEWS REPORTERS...",
-                                "THANK YOU BARBER SHOPS... THANK YOU TO ANYONE WHO THRIVES ON THE DOWNFALL OF OTHERS FOR I WILL NOT FALL!!!",
-                                "YOU FEEL MISERY IS THE ONLY COMPANY... AND I AM HERE TO PROVE YOU ALL WRONG...",
-                                "I'VE BEEN INVITED TO SHOWS AND AS USUAL ASKED NOT TO COME...",
-                                // "(SIGH) UURBAAAN (SIGH)",
-                                "I ACTUALLY HAVE BEEN WORKING ON MY EGO THOUGH.",
-                                "IT'S COOL TO TALK SHIT WHEN YOU'RE RAPPING BUT NOT IN REAL LIFE.",
-"HEY TWITTER, TAKE THE SO CALLED KANYE WEST TWITTER DOWN NOW... WHY?... BECAUSE MY CAPS LOCK KEY IS LOUD!!!!!!!!!",
-"EVERYTHING THAT TWITTER OFFERS I NEED LESS OF."
 
+// From KanyeUniverseCity.com blog posts
+"I am sick of negative people who just sit around trying 2 plot my downfall... Why????" => "http://www.kanyeuniversecity.com/blog/2008/06/untitled-2/",
+"HAS ANYONE EVEN TAKEN THE TIME TO AT LEAST DO THE MATH???" => "http://www.kanyeuniversecity.com/blog/2008/06/untitled-2/",
+"AT THAT POINT WE'RE RACING AGAINST THE SUN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" => "http://www.kanyeuniversecity.com/blog/2008/06/untitled-2/",
+"CALL ME WHAT YOU WANT BUT NEVER SAY I DIDN'T GIVE MY ALL!!!" => "http://www.kanyeuniversecity.com/blog/2008/06/untitled-2/",
+"ALL I CARE ABOUT ARE THE FANS." => "http://www.kanyeuniversecity.com/blog/2008/06/untitled-2/",
+"I'm typing so fucking hard I might break my fucking Mac book Air!!!!!!!!" => "", // this was lost when they moved to their new blog :\
+"I'VE BEEN ATTACKED FOR BEING ME... FOR BEING BRIGHT RED IN A GREY WORLD." => "http://cnn.com",
+"I AM NUCLEAR ENERGY" => "http://www.kanyeuniversecity.com/blog/2009/01/30/",
+"YOUR PESSIMISM IS MY POWER... YOUR PRESUMPTIONS LEAD TO MY REDEMPTIONS..." => "http://www.kanyeuniversecity.com/blog/2009/01/30/",
+"YOU OBVIOUSLY DON'T KNOW WHO YOU'RE UP AGAINST" => "http://www.kanyeuniversecity.com/blog/2009/01/30/",
+"I'VE BEEN LOVED AND HATED... I'VE BEEN HAILED AND RIDICULED..." => "http://www.kanyeuniversecity.com/blog/2009/01/30/",
+"I WILL NEVER ATTACK NEGATIVE PEOPLE AND THEIR ENERGY..." => "http://www.kanyeuniversecity.com/blog/2009/01/30/",
+"IT IS THE CRACK IN THE WALL THAT ALLOWS LIGHT TO SHINE THROUGH... SOOO THANK YOU SENSATIONAL NEWS REPORTERS..." => "http://www.kanyeuniversecity.com/blog/2009/01/30/",
+"THANK YOU BARBER SHOPS... THANK YOU TO ANYONE WHO THRIVES ON THE DOWNFALL OF OTHERS FOR I WILL NOT FALL!!!" => "http://www.kanyeuniversecity.com/blog/2009/01/30/",
+"YOU FEEL MISERY IS THE ONLY COMPANY... AND I AM HERE TO PROVE YOU ALL WRONG..." => "http://www.kanyeuniversecity.com/blog/2009/01/30/",
+"I AM A SOLDIER OF FREE THOUGHT IN A CLOSED MINDED WORLD AND I AM READY FOR WAR… " => "http://www.kanyeuniversecity.com/blog/2009/01/30/",
+"I'VE BEEN INVITED TO SHOWS AND AS USUAL ASKED NOT TO COME..." => "http://www.kanyeuniversecity.com/blog/2009/01/30/",
+"I ACTUALLY HAVE BEEN WORKING ON MY EGO THOUGH." => "http://www.kanyeuniversecity.com/blog/2009/04/4370/",
+"I’M SURE THERE’S GRAMMATICAL ERRORS IN THIS… THAT’S HOW YOU KNOW IT’S ME!" => "http://www.kanyeuniversecity.com/blog/2009/04/4370/",
+"IT'S COOL TO TALK SHIT WHEN YOU'RE RAPPING BUT NOT IN REAL LIFE." => "http://www.kanyeuniversecity.com/blog/2009/04/4370/",
+"HEY TWITTER, TAKE THE SO CALLED KANYE WEST TWITTER DOWN NOW... WHY?... BECAUSE MY CAPS LOCK KEY IS LOUD!!!!!!!!!" => "http://www.kanyeuniversecity.com/blog/2009/05/4901/",
+"EVERYTHING THAT TWITTER OFFERS I NEED LESS OF." => "http://www.kanyeuniversecity.com/blog/2009/05/4901/",
+
+
+// From his new @KanyeWest Twitter account
+"I ordered the salmon medium instead of medium well I didn't want to ruin the magic" => "http://twitter.com/kanyewest/status/19854009662",
+"Up early in the morning taking meetings in Silicone Valley" => "http://twitter.com/kanyewest/status/19752583676",
+"Classical music is tight yo" => "http://twitter.com/kanyewest/status/19835937717",
+"keep fresh flowers in the crib" => "http://twitter.com/kanyewest/status/19837801525",
+"I need this horse... Kings need horses" => "http://twitpic.com/29suqi"
 
                         );
 
